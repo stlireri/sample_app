a8b623134ba53400c5feee0899ff5697e6f898c5
stella <stellaireri@gmiail.com>
2016-11-06 00:00:36 +0300

    first red green refactor test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     assert_response :success
   end
+  
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
 
   test "should get about" do
     get static_pages_about_url
