ecc52b8ca6f3cc1b3b1bede8af2d8bdbea6401e6
stella <stellaireri@gmiail.com>
2017-01-31 14:21:05 +0300

    authenticate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 255 },
 format: { with: VALID_EMAIL_REGEX},
-	uniqueness:{case_sensitive: false}  
+	uniqueness:{case_sensitive: false} 
+	#attr_accessor :password, :password_confirmation
 has_secure_password
 validates :password, length:{minimum: 6 }
-end 
+end
